37cbbd05a753c9fbc1d6b4d971a1d042c622d733
IRAGUHA Pacifique <your.paciraguha.com>
2024-03-21 03:50:02 -0700

    well logged in

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGISTRATION</title>
+</head>
+<body>
+    <p1><i>Login Form</i></p1>
+    <form>
+   <Table>
+    <tr><td>Email</td><td><input type="text" name="email" ></td></tr> 
+    <tr><td>Password</td><td><input type="password" name="pass" ></td></tr> </tr>
+    <tr><td><input type="submit" name="submit" Value="Login"></td></tr>
+</form>
+</table>
+
+</body>
+</html>
